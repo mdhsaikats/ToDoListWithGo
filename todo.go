0d6522769b166f7e930cc5b7358787c29894f378
Saikat Sikder <popergyes2017@outlook.com>
2024-11-15 18:12:43 +0600

    secound commit

--- todo.go
+++ todo.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 )
 
-func add() {
-	
+func add(todolist *[]string) {
+
+	var item string
+	fmt.Print("Enter your task: ")
+	fmt.Scanln(&item)
+	*todolist = append(*todolist, item)
+	fmt.Print("Add Successfully\n")
+
 }
 func delete() {
 
@@ -14,6 +20,38 @@ func showall() {
 
 }
 func main() {
+	var todolist []string
 	fmt.Println("To Do List")
-	var arr [100]int
+	var option int
+
+	for {
+		fmt.Printf("1.Add\n2.Showlist\n3.Delete\n4.Exit\n")
+		fmt.Printf("Enter You Option: ")
+		fmt.Scanf("%d", &option)
+
+		switch option {
+		case 1:
+			{
+				add(&todolist)
+			}
+		case 2:
+			{
+				showall()
+			}
+		case 3:
+			{
+				delete()
+			}
+		case 4:
+			{
+				fmt.Print("Exiting....")
+				return
+			}
+		default:
+			{
+				fmt.Printf("Error!")
+			}
+		}
+	}
+
 }
