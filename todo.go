c84a24562784e0dd674e24e0a0bcaa1268502412
Saikat Sikder <popergyes2017@outlook.com>
2024-12-02 01:37:33 +0600

    implimenting delete function

--- todo.go
+++ todo.go
@@ -11,15 +11,23 @@ var todolist []string
 func add(item string) {
 	todolist = append(todolist, item)
 	fmt.Print("Added Successfully\n")
-
 }
 func showall() {
 	for i, item := range todolist {
 		fmt.Println(i, ": ", item)
 	}
 }
-func main() {
 
+func delete(index int) {
+	if index < 0 || index >= len(todolist) {
+		fmt.Println("Invalid task number.")
+		return
+	}
+	todolist = append(todolist[:index], todolist[index+1:]...)
+	fmt.Println("Task delete successfully.")
+
+}
+func main() {
 	fmt.Println("To Do List")
 	var option int
 	reader := bufio.NewReader(os.Stdin)
@@ -27,8 +35,12 @@ func main() {
 	for {
 		fmt.Printf("1.Add\n2.Showlist\n3.Delete\n4.Exit\n")
 		fmt.Printf("Enter You Option: ")
-		fmt.Scanf("%d", &option)
-
+		_, err := fmt.Scanf("%d", &option)
+		if err != nil {
+			fmt.Println("Invalid input, please enter a number.")
+			reader.ReadString('\n') // clear the input buffer
+			continue
+		}
 		switch option {
 		case 1:
 			{
@@ -44,6 +56,23 @@ func main() {
 		case 2:
 			{
 				showall()
+			}
+		case 3:
+			{
+				if len(todolist) == 0 {
+					fmt.Println("No tasks to delete.")
+					continue
+				}
+				fmt.Print("Enter task number to delete: ")
+				var index int
+				_, err := fmt.Scanf("%d", &index)
+				if err != nil {
+					fmt.Println("Invalid input.")
+					reader.ReadString('\n')
+					continue
+				}
+				delete(index)
+
 			}
 		default:
 			{
